98d97188fba5cac6e9771328958ce0ddde12380a
Sakthi Ganesh <96302038+sakthiganeshgithub@users.noreply.github.com>
2022-02-20 00:31:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+  var typed = new Typed(".typing", {
+        strings: [ "Developer", "Web Designer", "video editor","Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["Developer", "Web Designer", "video editor","freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
